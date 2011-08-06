1d494ba5750ec608183a93027c9fba1861d48bab
Aaron Steele <eightysteele@gmail.com>
2011-08-06 16:07:04 -0700

    Fix #14 by adding indexing stop words for properties and values.
    This commit adds stop words for property names and values to not
    index or full-text index. Full list: https://gist.github.com/1129865

--- vertnet/publishing/tools/bulkload_helper.py
+++ vertnet/publishing/tools/bulkload_helper.py
@@ -27,6 +27,58 @@ from google.appengine.ext.db import Expando
 from ndb import model
 from ndb import query
 
+STOP_WORDS = [
+    'a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 
+    'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 
+    'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 
+    'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 
+    'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 
+    'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 
+    'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 
+    'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 
+    'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 
+    'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 
+    'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 
+    'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your']
+
+# Darwin Core concept names whose value should not be full text indexed
+DO_NOT_FULL_TEXT = [
+    'acceptednameusageid', 'accessrights', 'basisofrecord', 'collectionid', 
+    'coordinateprecision', 'coordinateuncertaintyinmeters', 'datasetid', 
+    'dateidentified', 'day', 'decimallatitude', 'decimallongitude', 'disposition', 
+    'enddayofyear', 'eventdate', 'eventid', 'eventtime', 'fieldnotes', 
+    'footprintspatialfit', 'footprintsrs', 'footprintwkt', 'geologicalcontextid', 
+    'georeferenceremarks', 'georeferenceverificationstatus', 'highergeographyid', 
+    'identificationid', 'individualcount', 'individualid', 'institutionid', 
+    'language', 'locationid', 'maximumdepthinmeters', 
+    'maximumdistanceabovesurfaceinmeters', 'maximumelevationinmeters', 
+    'minimumdepthinmeters', 'minimumdistanceabovesurfaceinmeters', 
+    'minimumelevationinmeters', 'modified', 'month', 'nameaccordingtoid', 
+    'namepublishedinid', 'nomenclaturalcode', 'occurrencedetails', 'occurrenceid', 
+    'originalnameusageid', 'parentnameusageid', 'pointradiusspatialfit', 'rights', 
+    'rightsholder', 'scientificnameid', 'startdayofyear', 'taxonconceptid', 'taxonid', 
+    'type', 'verbatimcoordinates', 'verbatimeventdate', 'verbatimlatitude', 
+    'verbatimlongitude', 'year']
+
+# Darwin Core concept names that should not be indexed
+DO_NOT_INDEX = [
+    'acceptednameusageid', 'accessrights', 'associatedmedia', 
+    'associatedoccurrences', 'associatedreferences', 
+    'associatedsequences', 'associatedtaxa', 'bibliographiccitation', 
+    'collectionid', 'datageneralizations', 'datasetid', 'dateidentified', 
+    'disposition', 'eventdate', 'eventid', 'eventremarks', 'eventtime', 
+    'fieldnotes', 'footprintspatialfit', 'footprintsrs', 'footprintwkt', 
+    'geologicalcontextid', 'georeferenceremarks', 'georeferencesources', 
+    'habitat', 'higherclassification', 'highergeography', 'highergeographyid', 
+    'identificationid', 'identificationreferences', 'identificationremarks', 
+    'individualcount', 'individualid', 'informationwithheld', 'institutionid', 
+    'locationid', 'locationremarks', 'modified', 'nameaccordingtoid', 
+    'namepublishedin', 'namepublishedinid', 'occurrencedetails', 'occurrenceid', 
+    'occurrenceremarks', 'originalnameusageid', 'othercatalognumbers', 
+    'parentnameusageid', 'pointradiusspatialfit', 'preparations', 
+    'previousidentifications', 'rights', 'rightsholder', 'scientificnameid', 
+    'taxonconceptid', 'taxonid', 'taxonremarks', 'verbatimcoordinates', 
+    'verbatimlatitude', 'verbatimlongitude']
 
 def create_record_key():
     def wrapper(value, bulkload_state):
@@ -79,29 +131,47 @@ def get_corpus_list():
         Arguments:
             value - the JSON encoded record
         """
-        d = bulkload_state.current_dictionary
+        # Adds rejson properties to the current dictionary
+        # d = bulkload_state.current_dictionary
         recjson = simplejson.loads(value)
-        d.update(recjson)
-        bulkload_state.current_dictionary = d
-        corpus = set([x.strip().lower() for x in recjson.values()]) 
+        # d.update(recjson)
+        # bulkload_state.current_dictionary = d
+        
+        corpus = set(
+            [x.strip().lower() for key,x in recjson.iteritems() \
+                 if key.strip().lower() not in DO_NOT_FULL_TEXT and \
+                 x.strip().lower() not in STOP_WORDS]) 
         corpus.update(
             reduce(lambda x,y: x+y, 
                    map(lambda x: [s.strip().lower() for s in x.split() if s], 
-                       recjson.values()))) # adds tokenized values      
+                       [val for key,val in recjson.iteritems() \
+                            if key.strip().lower() not in DO_NOT_FULL_TEXT and \
+                            val.strip().lower() not in STOP_WORDS]))) # adds tokenized values      
         return list(corpus)
     return wrapper
 
 def add_dynamic_properties(input_dict, instance, bulkload_state_copy):    
-    for key,value in input_dict.iteritems():
+    """Adds dynamic properties from the CSV input_dict to the entity instance."""
+    # Adds rejson properties to the current dictionary
+
+    recjson = simplejson.loads(input_dict['recjson'])    
+    for key,value in recjson.iteritems():
+        if key in DO_NOT_INDEX:
+            continue
         try:
             instance[key] = value.lower()
         except:
             pass
-    instance.pop('rechash')
-    instance.pop('reckey')
-    recstate = instance.pop('recstate')
+
+    #instance.pop('rechash')
+    #instance.pop('reckey')
+    #instance.pop('recjson')
+    
+    # Do not bulkload CSV records that have recstate equal to 'deleted'
+    recstate = input_dict['recstate'] #instance.pop('recstate')
     if recstate == 'deleted':
         return datastore.Entity('RecordIndex')
+
     return instance
 
 if __name__ == '__main__':
